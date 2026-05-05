e8f0ed3cc1477111218bbb6e4f22c903f7e6c1fc
kennethlerch <kenneth.lerch@gmail.com>
2026-05-05 14:14:18 -0700

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -170,8 +170,25 @@ export default function RootLayout({
             gtag('config', 'AW-16844064763');
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PBSQZB4X');
+          `}
+        </Script>
       </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PBSQZB4X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <FooterCTAProvider>
           <Header />
           <main>{children}</main>
